2b4793958ae011fe35bdafb315ef16ed21fbe571
lukasg18 <lukas.gomes2010@gmail.com>
2019-05-02 17:24:36 -0300

    poblema do algorismo resolvido

--- src/ParserCalc.py
+++ src/ParserCalc.py
@@ -1,28 +1,50 @@
 """
-expr ::= term ((‘+’ | ‘-’) term)*
-term ::= factor ((‘*’ | ‘/’ | ‘//’ | ‘%’) factor)*
-factor ::= base (‘^’ factor)?
-base ::= (‘+’ | ‘-’) base | number | ‘(’ expr ‘)’
-number ::= digit+ ‘.’? digit* ((‘E’ | ‘e’) (‘+’ | ‘-’)? digit+)?
-digit ::= ‘0’ | ‘1’ | ‘2’ | ‘3’ | ‘4’ | ‘5’ | ‘6’ | ‘7’ | ‘8’ | ‘9’
+expr ::= term (('+' | '-') term)*
+term ::= factor (('*' | '/' | '//' | '%') factor)*
+factor ::= base ('^' factor)?
+base ::= base | '(' expr ')'
+digit ::= '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
 """
+
+from operator import floordiv, mod
 
 class ParserCalc():
     def __init__(self, tokens):
         self._tokens = tokens
         self._current = tokens[0]
+
+    def make_list(self, lst):
+        tokens = []
+        numbers = ''
+        operators = ''
+
+        for i in range(len(lst)):
+            if (self.digit(lst[i])):
+                numbers += lst[i]
+                if operators != '':
+                    tokens.append(operators)
+                    operators = ''
+            else:
+                operators += lst[i]
+                if numbers != '':
+                    tokens.append(numbers)
+                    numbers = ''
+        if (numbers != '' and operators == ''):
+            tokens.append(numbers)
+
+        return tokens
     
     def parser(self,lst):
         tokens = []
-        for i in range(len(lst)):
-            if lst[i].isdigit() and i > 0 and  (tokens[-1].isdigit() or tokens[-1][-1] is '.'):
-                # se ele encontrar 2 ou mais numeros em seguida ou um numero com '.', ele concatena  para uma unica posicao da lista       
-                tokens[-1] += lst[i] 
-            elif lst[i] is '.' and i > 0 and tokens[-1].isdigit():
-                tokens[-1] += lst[i]
-            else:
-                tokens.append(lst[i])
+        tokens = self.make_list(lst)
         print(ParserCalc(tokens).exp())
+
+    def next(self): 
+        #pula o primeiro elemento, pois ele ja foi processado
+        self._tokens = self._tokens[1:] 
+        if len(self._tokens) > 0:
+            # coloca o proximo elemento a ser processado
+            self._current = self._tokens[0]
 
     def exp(self):
         result = self.term()
@@ -37,18 +59,29 @@ class ParserCalc():
 
     def term(self):
         result = self.factor()
-        while self._current in ('*', '/'):
+        while self._current in ('*', '/', '%'):
             if self._current == '*':
                 self.next()
                 result *= self.term()
             if self._current == '/':
                 self.next()
                 result /= self.term()
+            if self._current == '%':
+                self.next()
+                result %= self.term()
         return result
 
     def factor(self):
+        result = self.base()
+        while self._current in ('^'):
+            if self._current == '^':
+                self.next()
+                result **= self.term()
+        return result
+
+    def base(self):
         result = None
-        if self._current[0].isdigit() or self._current[-1].isdigit():
+        if self.digit(self._current[0]) or self.digit(self._current[-1]):
             result = float(self._current)
             self.next()
         elif self._current is '(':
@@ -57,9 +90,9 @@ class ParserCalc():
             self.next()
         return result
 
-    def next(self): 
-        #pula o primeiro elemento, pois ele ja foi processado
-        self._tokens = self._tokens[1:] 
-        if len(self._tokens) > 0:
-            # coloca o proximo elemento a ser processado
-            self._current = self._tokens[0]
+    def digit(self, current):
+        digits = ['0','1','2','3','4','5','6','7','8','9']
+        if current in digits:
+            return True
+        else:
+            return False
